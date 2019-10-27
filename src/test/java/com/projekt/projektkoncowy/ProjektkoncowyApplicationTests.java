5193f6f85cea20b1dc06ffcfddfcb9064223f680
Blazej Rajkowski <blazejrajkowski@poczta.fm>
2019-10-27 14:52:37 +0100

    innitial commit

---
+++ src/test/java/com/projekt/projektkoncowy/ProjektkoncowyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projekt.projektkoncowy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektkoncowyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
